a68fa51ff6e4802043cd6ac9dc2f3affa571fa53
Konstantin Lomotin <ke.lomotin@gmail.com>
2017-10-26 00:14:05 +0200

    Продолжаются попытки запустить на плате
    Signed-off-by: Konstantin Lomotin <ke.lomotin@gmail.com>

--- board/labfor/labfor_top.v
+++ board/labfor/labfor_top.v
@@ -33,8 +33,8 @@ wire			clkIn = CLK;
 wire			rst_n = A2;
 wire			clkEnable = B2;
 wire	[3:0]	clkDevide = 4'b1010;
-wire	[4:0]	regAddr = {C1, B1, A1, D2, C2};
-wire	[31:0]	regData;
+wire	[4:0]	regAddr = {C1, B1, A1, D2, C2};	// Управляем выводом регистра с помощью пяти переключателей
+wire	[31:0]	regData;						// Выводимый регистр с номером regAddr
 wire 			clk_n;
 
 
@@ -45,22 +45,23 @@ sm_top sm_top
         .clkDevide  ( clkDevide ),
         .clkEnable  ( clkEnable ),
         .clk        ( clk_n     ),
-        .regAddr    ( regAddr   ),
-        .regData    ( regData   )
+        .regAddr    ( regAddr   ),	// Подключение адреса
+        .regData    ( regData   )	// Подключение вывода регистра
     );
 
 assign IND_1H = 1'b1;
 assign IND_2H = 1'b1;
 
-wire	[31:0]	h7segment = regData;
+wire	[31:0]	h7segment = regData;	// Вывод значения на другой wire
 wire	[6:0]	HEX0_D = {IND_1A, IND_1B, IND_1C, IND_1D, IND_1E, IND_1F, IND_1G};
 wire	[6:0]	HEX1_D = {IND_2A, IND_2B, IND_2C, IND_2D, IND_2E, IND_2F, IND_2G};
+
+assign LED[7:1] = h7segment[6:0];	// Вывод младших бит на светодиоды. 
+assign LED[0] = clk_n;				// Вывод тактового сигнала на первый светодиод
 
 sm_hex_display digit_1 ( h7segment [ 7: 4] , {IND_1A, IND_1B, IND_1C, IND_1D, IND_1E, IND_1F, IND_1G} );
 sm_hex_display digit_0 ( h7segment [ 3: 0] , {IND_2A, IND_2B, IND_2C, IND_2D, IND_2E, IND_2F, IND_2G} );
 
-assign LED[7:1] = regData[6:0];
-assign LED[0] = clk_n;
 
 
 endmodule 
